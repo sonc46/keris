f0f94411d96bb7156c334524c14b406aec1bda28
songayeon <sonc46@naver.com>
2020-10-05 16:32:21 +0900

    commit

--- app/resources/js/common.js
+++ app/resources/js/common.js
@@ -1,32 +1,54 @@
 //javascript document
 
-window.onload = function(){
+window.onload = function () {
 	var topMenuBtn = document.getElementById('topMenuBtn');
+
 	var topMenuShadow = document.getElementById('topMenuShadow');
 
-	topMenuShadow.addEventListener('click', function() {
+	topMenuShadow.addEventListener('click', function () {
 		topMenuBtn.click();
 	});
 
 	var translationSelectionBtn = document.getElementById('translationSelectionBtn');
 	var translationSelectionA = document.getElementsByClassName('translationSelectionA');
 
-	for(var i = 0; i < translationSelectionA.length; i++){
-		translationSelectionA[i].addEventListener('click', function() {
+	for (var i = 0; i < translationSelectionA.length; i++) {
+		translationSelectionA[i].addEventListener('click', function () {
 			translationSelectionBtn.click();
+			translationSelectionBtn.focus();
 		});
 	}
+	translationSelectionA[translationSelectionA.length - 1].addEventListener('focusout', function () {
+		selectionEvent();
+	})
+
+	var topmenu = document.getElementsByClassName('topmenu');
+	topmenu[topmenu.length - 1].addEventListener('focusout', function () {
+		topMenuBtn.focus();
+	})
 }
 
-var topMenuEvent = function() {
+var topMenuEvent = function () {
 	topMenuBtn.classList.toggle('active');
+	var topmenu = document.getElementsByClassName('topmenu');
+
+	if (topMenuBtn.classList.contains('active')) {
+
+		for (let i = 0; i < topmenu.length; i++) {
+			topmenu[i].setAttribute('href', topmenu[i].getAttribute('data-href'));
+		}
+
+	} else {
+		for (let i = 0; i < topmenu.length; i++) {
+			topmenu[i].removeAttribute('href');
+		}
+	}
+
 };
 
-var selectionEvent = function() {
+var selectionEvent = function () {
 	translationSelectionBtn.classList.toggle('active');
 };
-
-
 
 
 
